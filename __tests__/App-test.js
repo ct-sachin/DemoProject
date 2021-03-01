9b02cf9b8ddc59f55db48f6c7ace0631026460fc
Sachin Saini <sachin.saini@cleartax.in>
2021-03-01 23:56:58 +0530

    Test case written

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -9,6 +9,22 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const sum = (a,b) => a+b;
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+it('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it('knows that 2 cross 3 make 6', () => {
+    expect(2 * 3).toBe(8);
+  });
 });
