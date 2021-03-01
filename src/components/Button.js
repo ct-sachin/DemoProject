8df392bb214ef8f8568a46c09271e56a2d88aaa2
Sachin Saini <sachin.saini@cleartax.in>
2021-03-01 23:19:33 +0530

    Common button added

---
+++ src/components/Button.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+// interface ButtonProps {
+//   title: string;
+//   style?: object;
+//   onPress: (event: GestureResponderEvent) => void;
+//   type?: 'outline' | 'filled';
+// }
+
+const defaultstyle = StyleSheet.create({
+  container: {
+    margin: 20,
+    textAlign: 'center',
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    shadowColor: 'grey',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2,
+    elevation: 1,
+    height: 54,
+    width: 100,
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    lineHeight: 44,
+    fontFamily: '700',
+  },
+});
+
+const outlineStyles = {
+  container: {
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '3px',
+    shadowColor: '#d2d2d2',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: '3px',
+    elevation: 1,
+  },
+  text: {
+    color: 'blue',
+    fontSize: 16,
+    lineHeight: 44,
+    fontFamily: '700',
+  },
+};
+
+export const Button = ({title, style, onPress, type}) => {
+  const styles = type === 'outline' ? outlineStyles : defaultstyle;
+  return (
+    <TouchableOpacity style={[styles.container, style]} onPress={onPress}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
